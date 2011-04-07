20e62cd949586471a862e9bf3f6ed2382bc84333
Ly Minh Phuong <ly.minh.phuong.1403@gmail.com>
2011-04-07 18:49:44 +0700

    init

---
+++ src/main/java/org/exoplatform/social/plugin/youtubelink/Main.java
@@ -0,0 +1,13 @@
+package org.exoplatform.social.plugin.youtubelink;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
