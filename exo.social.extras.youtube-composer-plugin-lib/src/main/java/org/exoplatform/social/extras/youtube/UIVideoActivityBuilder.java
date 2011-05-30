2afa9131aaac1d6cc93c98866226723eb003e134
Hoat Le Van <hoatlevan@gmail.com>
2011-05-30 15:06:15 +0700

    #1 | Clean project layout

--- exo.social.extras.youtube-composer-plugin-lib/src/main/java/org/exoplatform/social/extras/youtube/UIVideoActivityBuilder.java
+++ exo.social.extras.youtube-composer-plugin-lib/src/main/java/org/exoplatform/social/extras/youtube/UIVideoActivityBuilder.java
@@ -3,14 +3,14 @@ package org.exoplatform.social.plugin.videolink;
 
 import java.util.Map;
 
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
 import org.exoplatform.social.core.activity.model.ExoSocialActivity;
 import org.exoplatform.social.webui.activity.BaseUIActivity;
 import org.exoplatform.social.webui.activity.BaseUIActivityBuilder;
-import org.exoplatform.services.log.ExoLogger;
-import org.exoplatform.services.log.Log;
 
 public class UIVideoActivityBuilder extends BaseUIActivityBuilder {
-	  private static final Log LOG = ExoLogger.getLogger(UIVideoActivityBuilder.class);
+	  private static final Log LOG = ExoLogger.getLogger(org.exoplatform.social.plugin.videolink.UIVideoActivityBuilder.class);
 	  
 	  @Override
 	  protected void extendUIActivity(BaseUIActivity uiActivity, ExoSocialActivity activity) {
